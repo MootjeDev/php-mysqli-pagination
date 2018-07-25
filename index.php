998212bb392184de1590733aeadc058801515249
Mootje <34894997+Revplit@users.noreply.github.com>
2018-07-25 18:47:21 +0200

    Checking connection
    // Check connection
    if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
    }

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
 <?php
 // connect to database
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
 mysqli_select_db($conn, 'pagination');
 // define how many results you want per page
 $results_per_page = 10;
